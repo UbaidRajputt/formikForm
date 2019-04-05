465d9e0e74d867ca2c107056b7e5ab919a080e59
UbaidRajputt <ubaidrajputt@gmail.com>
2019-04-05 12:12:25 +0500

    Code Format on custom Multipe select.js

--- src/components/customMultipeSelect.js
+++ src/components/customMultipeSelect.js
@@ -1,22 +1,20 @@
-import React,{Component} from 'react';
-import Select from 'react-select';
+import React, { Component } from "react";
+import Select from "react-select";
 
-class customMultipleSelect extends Component{
-state={
-
-}
-handleChange = value => {
+class customMultipleSelect extends Component {
+  state = {};
+  handleChange = value => {
     // this is going to call setFieldValue and manually update values.topcis
-    this.props.onChange('topics', value);
+    this.props.onChange("topics", value);
   };
 
   handleBlur = () => {
     // this is going to call setFieldTouched and manually update touched.topcis
-    this.props.onBlur('topics', true);
+    this.props.onBlur("topics", true);
   };
-render(){
-    return(
-        <div style={{ margin: '1rem 0' }}>
+  render() {
+    return (
+      <div style={{ margin: "1rem 0" }}>
         <label htmlFor="color">Topics (select at least 3) </label>
         <Select
           id="color"
@@ -28,12 +26,13 @@ render(){
           className="basic-multi-select"
           classNamePrefix="select"
         />
-        {!!this.props.error &&
-          this.props.touched && (
-            <div style={{ color: 'red', marginTop: '.5rem' }}>{this.props.error}</div>
-          )}
+        {!!this.props.error && this.props.touched && (
+          <div style={{ color: "red", marginTop: ".5rem" }}>
+            {this.props.error}
+          </div>
+        )}
       </div>
     );
-}
+  }
 }
 export default customMultipleSelect;

01afd532f83503536f423c93438fa0d57e028abf
UbaidRajputt <ubaidrajputt@gmail.com>
2019-04-05 12:10:28 +0500

    Code Formatting of phone Number.js

--- src/components/PhoneNumbers.js
+++ src/components/PhoneNumbers.js
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 import { Formik, Form, Field, FieldArray, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
-Yup.addMethod(Yup.object, 'uniqueProperty', function (propertyName, message) {
-  return this.test('unique', message, function (value) {
-    console.log('propert values is :',value[propertyName]);
+Yup.addMethod(Yup.object, "uniqueProperty", function(propertyName, message) {
+  return this.test("unique", message, function(value) {
+    console.log("propert values is :", value[propertyName]);
     if (!value || !value[propertyName]) {
-      
       return true;
     }
 
@@ -16,10 +15,12 @@ Yup.addMethod(Yup.object, 'uniqueProperty', function (propertyName, message) {
 
     const subOptions = options.slice(0, currentIndex);
 
-    if (subOptions.some((option) => option[propertyName] === value[propertyName])) {
+    if (
+      subOptions.some(option => option[propertyName] === value[propertyName])
+    ) {
       throw this.createError({
         path: `${path}.${propertyName}`,
-        message,
+        message
       });
     }
 
@@ -28,24 +29,25 @@ Yup.addMethod(Yup.object, 'uniqueProperty', function (propertyName, message) {
 });
 const schema = Yup.object().shape({
   phoneNumbers: Yup.array()
-  .of(
-      Yup.object().shape({
-        New: Yup.string()
-          .min(7, "too short")
-          .max(15, "Too long")
-          .required("Required")
-          ,
-        Recheck: Yup.string()
-          .oneOf([Yup.ref("New"), null], "Numbers dont match")
-          .required("Required")
-          .min(7, "too short")
-          .max(15, "Too long")
-      }).uniqueProperty('New','Number Alreay exists')
+    .of(
+      Yup.object()
+        .shape({
+          New: Yup.string()
+            .min(7, "too short")
+            .max(15, "Too long")
+            .required("Required"),
+          Recheck: Yup.string()
+            .oneOf([Yup.ref("New"), null], "Numbers dont match")
+            .required("Required")
+            .min(7, "too short")
+            .max(15, "Too long")
+        })
+        .uniqueProperty("New", "Number Alreay exists")
     )
     .required("Must have Phone Number") // these constraints are shown if and only if inner constraints are satisfie
     .min(1, "Minimum of 1 Phone Numbers")
-    .max(5, "Maximum of 5 Phone Numbers") 
-})
+    .max(5, "Maximum of 5 Phone Numbers")
+});
 class PhoneNumber extends Component {
   state = {
     error: false
@@ -64,8 +66,7 @@ class PhoneNumber extends Component {
             }, 500)
           }
           validationSchema={schema}
-          render={({ values,errors }) => (
-            
+          render={({ values, errors }) => (
             <Form>
               <FieldArray
                 name="phoneNumbers"
@@ -83,7 +84,7 @@ class PhoneNumber extends Component {
                           type="number"
                         />
                         <ErrorMessage name={`phoneNumbers.${index}.Recheck`} />
-                        
+
                         <button
                           type="button"
                           onClick={() => arrayHelpers.remove(index)}

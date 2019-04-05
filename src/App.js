861419f6c3e582945e0952ff25109a4fec8f1e1c
UbaidRajputt <ubaidrajputt@gmail.com>
2019-04-05 09:54:41 +0500

    Define routes for Form & About

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
-import MyEnhancedForm from './components/withFormikMainForm'; 
-
+import React, { Component } from "react";
+import MyEnhancedForm from "./components/withFormikMainForm";
+import About from './components/About';
+import { Route, Switch,Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 class App extends Component {
   render() {
     return (
-      <div >
-        <MyEnhancedForm/>
+      <div>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand>Formik Forms</Navbar.Brand>
+          </Navbar>
+          <ul>
+           <li><Link to="/Form">Form</Link></li>
+           <li><Link to="/About">About Us</Link></li>
+         </ul>
+            <Switch>
+              <Route path="/Form" name="Form" component={MyEnhancedForm} />
+              <Route path="/About" name="About" component={About} />
+            </Switch>
       </div>
     );
   }
+  
 }
 
 export default App;

cdb58c3bcd425a7d8e1be3cbbae3cba1b5dacec4
UbaidRajputt <ubaidrajputt@gmail.com>
2019-04-05 09:55:54 +0500

    Add support of 3languages

--- src/components/singleDatePicker.js
+++ src/components/singleDatePicker.js
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 import { SingleDatePicker } from "react-dates";
 import moment from "moment";
 import isInclusivelyAfterDay from "moment";
+import idLocale from "moment/locale/id";
+import esLocale from "moment/locale/es";
 
 class SingleDater extends Component {
   constructor(props) {
     super(props);
+    this.someMethod = this.someMethod.bind(this);
     this.range(1900, 2019);
   }
   state = {
     date: null,
     focused: null,
-    yearArr: []
+    yearArr: [],
+    langSelection: "Date"
   };
   range(start, end) {
     for (let i = start; i <= end; i++) {
@@ -27,7 +31,9 @@ class SingleDater extends Component {
           onChange={e => onMonthSelect(month, e.target.value)}
         >
           {moment.months().map((label, value) => (
-            <option key={value} value={value}>{label}</option>
+            <option key={value} value={value}>
+              {label}
+            </option>
           ))}
         </select>
       </div>
@@ -47,9 +53,17 @@ class SingleDater extends Component {
   );
 
   render() {
+    if (this.state.langSelection === "Tanggal") {
+      moment.updateLocale("id", idLocale);
+    } else if (this.state.langSelection === "Fecha") {
+      moment.updateLocale("es", esLocale);
+    } else {
+      moment.locale("en");
+    }
     return (
       <div>
         <SingleDatePicker
+          placeholder={this.state.langSelection}
           id={this.props.field.name} // momentPropTypes.momentObj or null
           date={this.state.date}
           onDateChange={date => {
@@ -62,8 +76,17 @@ class SingleDater extends Component {
           renderMonthElement={this.renderMonthElement}
           isOutsideRange={day => !isInclusivelyAfterDay(day, moment())}
         />
+        <select onChange={this.someMethod}>
+          <option value="Date">select an option</option>
+          <option value="Tanggal">Indonesian</option>
+          <option value="Fecha">Spanish</option>
+        </select>
       </div>
     );
+  }
+  someMethod(e) {
+    this.setState({ langSelection: e.target.value }, () => {
+    });
   }
 }
 

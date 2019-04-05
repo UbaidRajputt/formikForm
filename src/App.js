1a3cad02fa38f97a27e87098856276e0496a1fd8
UbaidRajputt <ubaidrajputt@gmail.com>
2019-04-05 12:13:05 +0500

    Code Format on App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import MyEnhancedForm from "./components/withFormikMainForm";
-import About from './components/About';
-import { Route, Switch,Link } from "react-router-dom";
+import About from "./components/About";
+import { Route, Switch, Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 class App extends Component {
   render() {
@@ -9,19 +9,22 @@ class App extends Component {
       <div>
         <Navbar bg="primary" variant="dark">
           <Navbar.Brand>Formik Forms</Navbar.Brand>
-          </Navbar>
-          <ul>
-           <li><Link to="/Form">Form</Link></li>
-           <li><Link to="/About">About Us</Link></li>
-         </ul>
-            <Switch>
-              <Route path="/Form" name="Form" component={MyEnhancedForm} />
-              <Route path="/About" name="About" component={About} />
-            </Switch>
+        </Navbar>
+        <ul>
+          <li>
+            <Link to="/Form">Form</Link>
+          </li>
+          <li>
+            <Link to="/About">About Us</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/Form" name="Form" component={MyEnhancedForm} />
+          <Route path="/About" name="About" component={About} />
+        </Switch>
       </div>
     );
   }
-  
 }
 
 export default App;
